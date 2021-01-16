a09e7ad69526a260037d86ea06264c1a805e0316
Mauricio Figueroa <mfigueroapa@hotmail.com>
2021-01-16 16:50:02 -0600

    delete product service from client

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useState, useEffect } from "react"
-import { findAll } from "../../services/product"
+import { findAll, deleteProduct } from "../../services/product"
 import "./Home.scss"
 
 const Home = () => {
@@ -15,24 +15,38 @@ const Home = () => {
   }, [])
   console.log(products)
 
+  const deleteHandle = (id) => {
+    deleteProduct(id)
+      .then((response) => {
+        console.log(response)
+        setProducts(products.filter(product => product.id !== id))
+      })
+      .catch((err) => console.log(err))
+  }
+
   return (
     <div className="home">
       <div className="home-container">
         {products ? (
           <div className="products-container">
             <div className="products-titles">
-              <b className='product-title'>PRODUCT&nbsp;</b>
-              <b className='description-title'>DESCRIPTION&nbsp;</b>
+              <b className="product-title">PRODUCT&nbsp;</b>
+              <b className="description-title">DESCRIPTION&nbsp;</b>
             </div>
             {products.map((product) => (
               <div className="products" key={product.id}>
                 <div className="product-info">
                   <li>
-                    <b className='product-name'>{product.name}</b>&nbsp;
+                    <b className="product-name">{product.name}</b>&nbsp;
                   </li>
-                  <li className='product-description'>{product.description}&nbsp;</li>
+                  <li className="product-description">
+                    {product.description}&nbsp;
+                  </li>
                 </div>
-                <i className="fas fa-trash-alt"></i>
+                <i
+                  onClick={() => deleteHandle(product.id)}
+                  className="fas fa-trash-alt"
+                ></i>
               </div>
             ))}
           </div>
